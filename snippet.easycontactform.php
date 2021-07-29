3ece862bf4abd74f60bd6e6131a3c23cdc8c8fef
dmitryserenko <dmitry@serenko.ru>
2021-07-29 23:39:47 +0300

    Update snippet.easycontactform.php

--- snippet.easycontactform.php
+++ snippet.easycontactform.php
@@ -103,9 +103,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['token']) && $_POST['to
                     $(\'#simpleсontactResult\').html(result);
                 })
                 .fail(function() {
-                    $(\'#message\').html(\'<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#00b51a" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg><p class="h4">Error</p><p class="help-text margin-bottom-2">К сожалению возникла ошибка, попробуйте повторить позже</p><a class="button hollow" data-close>Закрыть</a>\');
-                    $(\'#message\').append(\'<button class="close-button" data-close type="button"><span aria-hidden="true">&times;</span></button>\');
-                    $(\'#message\').foundation(\'open\');
+                    $(\'#simpleсontactResult\').html(\'Error\');
                 });
         } else {$(\'#simpleсontactResult\').html(\'<input type="button" class="simpleсontact_button button" value="Отправить" disabled />\');}
     });
